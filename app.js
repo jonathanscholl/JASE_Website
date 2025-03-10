17a043839609aedc6e3e0a80cfb0244e565a3d9f
jonischolll <152484485+jonischolll@users.noreply.github.com>
2025-03-10 17:19:57 +0100

    added new files

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import morgan from "morgan";
+
+
+const app = express()
+const PORT = 3000
+
+
+app.use(morgan("tiny"))
+
+app.use('/public', express.static('public'))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+
+
+
+app.listen(PORT,() => {
+
+    console.log("Server started")
+})
+
+app.get('/', (request, response) => {
+    response.render('index')
+  })
+
+  app.get('/about', (request, response) => {
+    response.render('about')
+  })
+
+  app.get('/contact', (request, response) => {
+    response.render('contact')
+  })
+
+
+  app.post('/contact', (request, response) => {
+    console.log('Contact form submission: ', request.body.Name)
+    const name = request.body.Name
+    const template_message = `Thank you for your message ${name}.`
+    response.render('template', {template_message: template_message})
+  })
+
+
+
+  app.get('*', (request, response) => {
+
+    const url = request.url
+    response.render('404_notfound', {url: url})
+  })
+
+
+  
+
+
