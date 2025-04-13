dd1d66a686aaea71bff642d9b6458fca5b829920
jonischolll <152484485+jonischolll@users.noreply.github.com>
2025-04-13 16:07:24 +0200

    (refactor) moved feedback database handling to models->feedbackModels

---
+++ models/feedbackModel.js
@@ -0,0 +1,89 @@
+import e from "express";
+import { supabase } from "../services/supabase.js";
+
+
+export const getFeedback = async() => {
+
+
+    const { data, error } = await supabase
+    .from("feedback")
+    .select("id, message, votes, profile_id")
+    .order('votes', { ascending: false })
+
+
+    if (error) {
+
+        console.log(error)
+        return response.status(401).json({ error: error });
+    }
+
+    return data
+}
+
+
+export const postFeedback = async(message, profile_id) => {
+
+    const {error} = await supabase
+    .from("feedback")
+    .insert({
+      message: message,
+      profile_id: profile_id
+    })
+
+    if (error) {
+
+        console.log(error)
+    }
+
+
+}
+
+
+export const getFeedbackVotes = async (id) => {
+
+    const { data, error } = await supabase
+    .from("feedback")
+    .select("votes")
+    .eq("id", id)
+    .single();
+
+    if (error) {
+
+        console.log(error)
+    }
+
+    return data
+}
+
+
+export const updateFeedbackVotes = async (newVotes) => {
+
+    const { error } = await supabase
+.from("feedback")
+.update({ votes: newVotes })
+.eq("id", id);
+
+if (error) {
+
+
+    console.log(error)
+}
+
+}
+
+
+export const deleteFeedback = async (id) => {
+
+
+    const { data, error } = await supabase
+    .from("feedback")
+    .delete()
+    .eq("id", id)
+
+    if (error) {
+
+        console.log(error)
+    }
+}
+
+
