cdabed3d7d74272a91d628f4f59842cf411c4e96
jonischolll <152484485+jonischolll@users.noreply.github.com>
2025-05-23 11:53:53 +0200

    (fix) routing of feedback post

--- routes/feedbackRoutes.js
+++ routes/feedbackRoutes.js
@@ -2,8 +2,6 @@ import express from "express";
 
 import { showFeedback, postFeedback, upvoteFeedback, handleDeleteFeedback } from "../controllers/feedbackController";
 
-
-
 const router = express.Router();
 
 router.get('/', showFeedback);
@@ -13,7 +11,6 @@ router.post('/', postFeedback);
 router.post('/upvote', upvoteFeedback);
 
 router.post('/delete', handleDeleteFeedback);
-
 
 export default router;
 
